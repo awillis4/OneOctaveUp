eb076156489e21b0e8fbf848798b1eb060de954d
Armani Willis <45676506+awillis4@users.noreply.github.com>
2020-03-28 20:36:10 -0400

    Add files via upload
    Seamlessly and easily allows a user to install and load any amount of the 92 packages in octave forge.
    Created because it would then be very easy to install and load any packafe from forge.

---
+++ octaveup.m
@@ -0,0 +1,116 @@
+pkg install -forge acturial;pkg load acturial; 
+pkg install -forge arduino;pkg load arduino;
+pkg install -forge audio;pkg load audio;
+
+pkg install -forge bim;pkg load bim;
+pkg install -forge bsltl;pkg load bsltl;
+
+pkg install -forge cgi;pkg load cgi; 
+pkg install -forge communications;pkg load communications;
+pkg install -forge combinatorics;pkg load combinatorics; 
+pkg install -forge control;pkg load control;
+
+pkg install -forge database;pkg load database;
+pkg install -forge dataframe;pkg load dataframe; 
+pkg install -forge data-smoothing;pkg load data-smoothing;
+pkg install -forge dicom;pkg load dicom;
+pkg install -forge divand;pkg load divand;
+pkg install -forge doctest;pkg load doctest;
+
+pkg install -forge econometrics;pkg load econometrics;
+pkg install -forge es;pkg load es;
+
+pkg install -forge fem-fenics;pkg load fem-fenics;
+pkg install -forge fenv;pkg load fenv;  
+pkg install -forge financial;pkg load financial;
+pkg install -forge fits;pkg load fits;
+pkg install -forge fl-core;pkg load fl-core; 
+pkg install -forge fpl;pkg load fpl;
+pkg install -forge fuzzy-logic-toolkit;pkg load fuzzy-logic-toolkit;
+
+pkg install -forge ga;pkg load ga;
+pkg install -forge general;pkg load general;
+pkg install -forge generate_html;pkg load generate_html;
+pkg install -forge geometry;pkg load geometry;
+pkg install -forge gpc;pkg load gpc;
+pkg install -forge gnuplot;pkg load gnuplot; 
+pkg install -forge gsl;pkg load gsl;
+
+pkg install -forge image;pkg load image;
+pkg install -forge image-acquisition;pkg load image-acquisition;
+pkg install -forge instrument-control;pkg load instrument-control;
+pkg install -forge interval;pkg load interval;
+pkg install -forge io;pkg load io;
+
+pkg install -forge java;pkg load java;
+
+pkg install -forge level-set;pkg load level-set;
+pkg install -forge linear-algebra;pkg load linear-algebra;
+pkg install -forge lssa;pkg load lssa; 
+pkg install -forge ltfat;pkg load ltfat;
+
+pkg install -forge mapping;pkg load mapping;
+pkg install -forge matgeom;pkg load matgeom;
+pkg install -forge miscellaneous;pkg load miscellaneous;
+pkg install -forge mechanics;pkg load mechanics;
+pkg install -forge mpi;pkg load mpi; 
+pkg install -forge msh;pkg load msh; 
+pkg install -forge mvn;pkg load mvn;
+
+pkg install -forge nan;pkg load nan;
+pkg install -forge NaN;pkg load NaN;
+pkg install -forge ncarray;pkg load ncarray;
+pkg install -forge netcdf;pkg load netcdf;
+pkg install -forge nlwing;pkg load nlwing;
+pkg install -forge nnet;pkg load nnet;
+pkg install -forge nurbs;pkg load nurbs;
+
+pkg install -forge ocl;pkg load ocl;
+pkg install -forge ocs;pkg load ocs;
+pkg install -forge octcdf;pkg load octcdf;
+pkg install -forge octclip;pkg load octclip;
+pkg install -forge octgpr;pkg load octgpr; 
+pkg install -forge octproj;pkg load octproj;
+pkg install -forge ode;pkg load ode; 
+pkg install -forge odepkg;pkg load odepkg;
+pkg install -forge openmpi_ext;pkg load openmpi_ext; 
+pkg install -forge optics;pkg load optics;
+pkg install -forge optim;pkg load optim;
+pkg install -forge optiminterp;pkg load optiminterp;
+
+pkg install -forge parallel;pkg load parallel;
+pkg install -forge physicalconstants;pkg load physicalconstants; 
+pkg install -forge plot;pkg load plot;
+pkg install -forge pt-br;pkg load pt-br;
+
+pkg install -forge quaternion;pkg load quaternion; 
+pkg install -forge queueing;pkg load queueing;
+
+pkg install -forge secs1d;pkg load secs1d;
+pkg install -forge signal;pkg load signal
+pkg install -forge sockets;pkg load sockets;
+pkg install -forge sparsersb;pkg load sparsersb;
+pkg install -forge specfun;pkg load specfun;
+pkg install -forge splines;pkg load splines;
+pkg install -forge statistics;pkg load statistics;
+pkg install -forge stk;pkg load stk;
+pkg install -forge strings;pkg load strings;
+pkg install -forge struct;pkg load struct;
+pkg install -forge symbolic;pkg load symbolic;
+
+pkg install -forge time;pkg load time;
+pkg install -forge tisean;pkg load tisean;
+pkg install -forge tsa;pkg load tsa;
+
+pkg install -forge vibes;pkg load vibes; 
+pkg install -forge video;pkg load video;
+pkg install -forge vrml;pkg load vrml;
+
+pkg install -forge windows;pkg load windows;
+
+pkg install -forge zeromq;pkg load zeromq;
+ 
+% An alphabetically sorted matlab file of packages 
+% meant to install and load 92 packages 
+% Simply comment out packages you do not want with %
+% precompiled so as not avoid nuisance of searching 4 packages.
